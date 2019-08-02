59cec330343693a9120825bdf36d1dba4040bbfd
lonwern <lonwern@gmail.com>
2019-08-02 10:40:45 +0800

    update

--- my-antigen.zsh
+++ my-antigen.zsh
@@ -4,14 +4,16 @@ source $(cd `dirname $0`; pwd)/antigen.zsh
 antigen use oh-my-zsh
 
 # Bundles from the default repo (robbyrussell's oh-my-zsh).
-antigen bundle git
-antigen bundle z
 antigen bundle docker
 antigen bundle docker-compose
+antigen bundle git
+antigen bundle helm
+antigen bundle z
 
 # Bundles from zsh-users.
-antigen bundle zsh-users/zsh-syntax-highlighting
 antigen bundle zsh-users/zsh-autosuggestions
+antigen bundle zsh-users/zsh-completions
+antigen bundle zsh-users/zsh-syntax-highlighting
 
 # Load the theme.
 antigen theme ys
